55b5ae04dd8d9cea57b2e6046b38bb7a213fec06
frixaco <rustam21ashurmatov@gmail.com>
2022-01-31 10:58:10 +0500

    Added restart, win-lose, eat-grow logic

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Snake game made with vanilla JavaScript
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("game");
-const rowCount = 11;
-const colCount = 11;
+const rowCount = 31;
+const colCount = 31;
 
 for (let r = 0; r < rowCount; r++) {
   const row = document.createElement("div");
@@ -30,6 +30,20 @@ let snake = [
 ];
 
 function initSnake() {
+  [].slice.call(container.children).forEach((row) => {
+    [].slice.call(row.children).forEach((col) => {
+      col.style.backgroundColor = "white";
+    });
+  });
+
+  snake = [
+    [5, 6],
+    [6, 6],
+    [7, 6],
+    [7, 5],
+    [7, 4],
+  ];
+
   snake.forEach((part) => {
     const cell = getCellAtPosition(part);
     cell.style.backgroundColor = "red";
@@ -60,62 +74,87 @@ const downPos = (pos) => [pos[0] + 1, pos[1]];
 const upPos = (pos) => [pos[0] - 1, pos[1]];
 const leftPos = (pos) => [pos[0], pos[1] - 1];
 const rightPos = (pos) => [pos[0], pos[1] + 1];
-
+let lost = false;
 function move(posSetter) {
+  if (lost) {
+    return;
+  }
+
   if (
     snake[0][0] === 1 ||
     snake[0][0] === rowCount ||
     snake[0][1] === 1 ||
     snake[0][1] === colCount
   ) {
+    lost = true;
+    alert("You lost");
     return;
   }
 
-  // if (
-  //   getCellAtPosition(posSetter(snake[0])).style.backgroundColor === "green"
-  // ) {
-  //   const end = [...snake].reverse()[0];
-  //   const add = [end[0] + 1, end[1]];
-  //   snake.push(add);
-  //   getCellAtPosition(add).style.backgroundColor = "red";
-  // }
-
   let prev = snake[0];
-  snake = snake.map((curr, index) => {
+  let food = false;
+
+  let newSnake = [];
+  snake.forEach((curr, index) => {
     if (index === 0) {
       prev = curr;
       const next = posSetter(curr);
 
-      getCellAtPosition(next).style.backgroundColor = "red";
-      return next;
-    }
+      if (getCellAtPosition(next).style.backgroundColor === "green") {
+        food = true;
+      }
 
-    const next = prev;
-    prev = curr;
-    getCellAtPosition(prev).style.backgroundColor = "white";
-    getCellAtPosition(next).style.backgroundColor = "red";
-    return next;
+      getCellAtPosition(next).style.backgroundColor = "red";
+      newSnake.push(next);
+    } else {
+      const next = prev;
+      prev = curr;
+
+      if (food && index === snake.length - 1) {
+        getCellAtPosition(snake[snake.length - 1]).style.backgroundColor =
+          "red";
+        newSnake.push(next);
+        getCellAtPosition(next).style.backgroundColor = "red";
+        newSnake.push(prev);
+      } else {
+        getCellAtPosition(prev).style.backgroundColor = "white";
+        getCellAtPosition(next).style.backgroundColor = "red";
+        newSnake.push(next);
+      }
+    }
   });
+
+  if (
+    snake
+      .slice(1)
+      .filter((part) => part[0] === snake[0][0] && part[1] === snake[0][1])
+      .length > 0
+  ) {
+    alert("You lost");
+    lost = true;
+    return;
+  }
+  snake = [...newSnake];
 }
 
 document.addEventListener("keydown", (ev) => {
   if (ev.key === "ArrowDown") {
     clearTimers();
-    down = setInterval(() => move(downPos), 200);
+    down = setInterval(() => move(downPos), 100);
   }
 
   if (ev.key === "ArrowUp") {
     clearTimers();
-    up = setInterval(() => move(upPos), 200);
+    up = setInterval(() => move(upPos), 100);
   }
 
   if (ev.key === "ArrowLeft") {
     clearTimers();
-    left = setInterval(() => move(leftPos), 200);
+    left = setInterval(() => move(leftPos), 100);
   }
 
   if (ev.key === "ArrowRight") {
     clearTimers();
-    right = setInterval(() => move(rightPos), 200);
+    right = setInterval(() => move(rightPos), 100);
   }
 });
